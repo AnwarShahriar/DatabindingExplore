714fbc94187a424de9478f790e100a52f904bc57
anwarshahriar <anwarshahriar@gmail.com>
2016-11-14 01:00:09 +0600

    Enable android databinding and replace findviewbyid with binding

--- app/src/main/java/me/anwarshahriar/databindingexplore/MainActivity.java
+++ app/src/main/java/me/anwarshahriar/databindingexplore/MainActivity.java
@@ -1,13 +1,21 @@
 package me.anwarshahriar.databindingexplore;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import me.anwarshahriar.databindingexplore.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding =
+                DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.tvOne.setText("Hello One");
+        binding.tvTwo.setText("Hello Two");
+        binding.tvThree.setText("Hello Three");
+
     }
 }
