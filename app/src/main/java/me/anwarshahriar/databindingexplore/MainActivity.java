8540b73da8c22e51e1ed59b5f030a407f6fa2186
anwarshahriar <anwarshahriar@gmail.com>
2016-11-27 15:05:51 +0600

    Demo for databinding expression language

--- app/src/main/java/me/anwarshahriar/databindingexplore/MainActivity.java
+++ app/src/main/java/me/anwarshahriar/databindingexplore/MainActivity.java
@@ -4,6 +4,12 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import me.anwarshahriar.databindingexplore.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +20,21 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding =
                 DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        binding.setAStudent(new Student("Shahriar Anwar", 28));
+        Student aStudent = new Student("Shahriar Anwar", 28);
+        binding.setAStudent(aStudent);
         binding.setHandler(new EventHandler());
+
+        Student jamal = new Student("Jamal Ahmed", 15);
+
+        List<Student> students = new ArrayList<>();
+        students.add(jamal);
+
+        Map<String, Integer> nameVsAge = new HashMap<>();
+        nameVsAge.put(jamal.getName(), jamal.getAge());
+
+        binding.setStudents(students);
+        binding.setNameVsAge(nameVsAge);
+        binding.setShowStar(false);
     }
 
     public class EventHandler {
