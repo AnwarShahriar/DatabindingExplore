fc22e5ce9c4f17b162300f15f399475609bc7d7c
anwarshahriar <anwarshahriar@gmail.com>
2016-12-03 17:49:49 +0600

    Demo for image loading with binding adapter and glide

--- app/src/main/java/me/anwarshahriar/databindingexplore/MainActivity.java
+++ app/src/main/java/me/anwarshahriar/databindingexplore/MainActivity.java
@@ -1,14 +1,12 @@
 package me.anwarshahriar.databindingexplore;
 
+import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.ImageView;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.bumptech.glide.Glide;
 
 import me.anwarshahriar.databindingexplore.databinding.ActivityMainBinding;
 
@@ -20,32 +18,16 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding =
                 DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        Student aStudent = new Student("Shahriar Anwar", 28);
+        Student aStudent = new Student("Shahriar Anwar", 999);
+        aStudent.setImageUrl("https://lh3.googleusercontent.com/ET01Ksz0wvjTIZD-3pWezJNQvYABuhKY8XiujK4zFJHNBMPhBv7hXxrWSLLfyhPg0oLy3OfL=w1366-h768-rw-no");
+
         binding.setAStudent(aStudent);
-        binding.setHandler(new EventHandler());
-
-        Student jamal = new Student("Jamal Ahmed", 15);
-
-        List<Student> students = new ArrayList<>();
-        students.add(jamal);
-
-        Map<String, Integer> nameVsAge = new HashMap<>();
-        nameVsAge.put(jamal.getName(), jamal.getAge());
-
-        binding.setStudents(students);
-        binding.setNameVsAge(nameVsAge);
-        binding.setShowStar(false);
-    }
-
-    public class EventHandler {
-        public void handleClick(String name) {
-            Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
-        }
     }
 
     public class Student {
         private String name;
         private int age;
+        private String imageUrl;
 
         public Student(String name, int age) {
             this.name = name;
@@ -59,5 +41,22 @@ public class MainActivity extends AppCompatActivity {
         public int getAge() {
             return age;
         }
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
     }
+
+    @BindingAdapter("imageUrl")
+    public static void loadImage(ImageView iv, String url) {
+        Glide.with(iv.getContext())
+                .load(url)
+                .fitCenter()
+                .into(iv);
+    }
+
 }
