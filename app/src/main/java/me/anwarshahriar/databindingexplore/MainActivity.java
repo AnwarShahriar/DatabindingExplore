1ffd93f4a20a5b0af5999911e2aa98f42ff0dc2a
anwarshahriar <anwarshahriar@gmail.com>
2016-12-23 04:46:25 +0600

    Complete the demo

--- app/src/main/java/me/anwarshahriar/databindingexplore/MainActivity.java
+++ app/src/main/java/me/anwarshahriar/databindingexplore/MainActivity.java
@@ -1,6 +1,9 @@
 package me.anwarshahriar.databindingexplore;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import me.anwarshahriar.databindingexplore.databinding.ActivityMainBinding;
@@ -10,9 +13,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding =
+        final ActivityMainBinding binding =
                 DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        ViewModel model = new ViewModel();
+        model.setColor(Color.BLACK);
+        binding.setModel(model);
+    }
+
+    public class ViewModel extends BaseObservable {
+        private int color;
+
+        public void setColor(int color) {
+            this.color = color;
+            notifyPropertyChanged(BR.color);
+        }
+
+        @Bindable
+        public int getColor() {
+            return color;
+        }
     }
 
 }
